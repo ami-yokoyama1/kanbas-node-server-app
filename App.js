85bb285718401859a3e166ef78b5b7d71757c34f
ami-yokoyama1 <ami.yokoyama1@gmail.com>
2024-04-08 13:49:16 -0700

    noode

--- App.js
+++ App.js
@@ -1,21 +1,9 @@
-// const express = require('express');
-// const app = express()
-
-// function sayHello(req, res) {
-//     res.send("Hello World");
-// }
-
-// app.get('/hello', sayHello);
-
-// app.listen(4000);
-
 import express from 'express';
 import Hello from './Hello.js';
 import Lab5 from './Lab5.js';
 import cors from "cors";
-import CourseRoutes from "./Kanbas/courses/routes.js";
-import ModuleRoutes from "./Kanbas/modules/routes.js";
-
+import CourseRoutes from "./Kanbas/Courses/routes.js";
+import ModuleRoutes from "./Kanbas/Modules/routes.js";
 const app = express();
 app.use(cors());
 CourseRoutes(app);
@@ -23,4 +11,4 @@ app.use(express.json());
 ModuleRoutes(app);
 Lab5(app);
 Hello(app);
-app.listen(4000);
+app.listen(process.env.PORT ||4000);
--- Kanbas/modules/routes.js
+++ Kanbas/modules/routes.js
@@ -1,20 +1,11 @@
 import db from "../Database/index.js";
 function ModuleRoutes(app) {
-  app.put("/api/modules/:mid", (req, res) => {
-    const { mid } = req.params;
-    const moduleIndex = db.modules.findIndex(
-      (m) => m._id === mid);
-    db.modules[moduleIndex] = {
-      ...db.modules[moduleIndex],
-      ...req.body
-    };
-    res.sendStatus(204);
-  });
   app.delete("/api/modules/:mid", (req, res) => {
     const { mid } = req.params;
     db.modules = db.modules.filter((m) => m._id !== mid);
     res.sendStatus(200);
   });
+  
   app.post("/api/courses/:cid/modules", (req, res) => {
     const { cid } = req.params;
     const newModule = {
@@ -30,6 +21,16 @@ function ModuleRoutes(app) {
     const modules = db.modules
       .filter((m) => m.course === cid);
     res.send(modules);
+  });
+  app.put("/api/modules/:mid", (req, res) => {
+    const { mid } = req.params;
+    const moduleIndex = db.modules.findIndex(
+      (m) => m._id === mid);
+    db.modules[moduleIndex] = {
+      ...db.modules[moduleIndex],
+      ...req.body
+    };
+    res.sendStatus(204);
   });
 }
 export default ModuleRoutes;
