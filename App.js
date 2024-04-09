55f5e72955bb3a749723755ef3b31cab3c1d68c9
ami-yokoyama1 <ami.yokoyama1@gmail.com>
2024-04-08 21:05:27 -0700

    typo

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import mongoose from "mongoose";
 
-mongoose.connect("congodb://127.0.0.1:27017/kanbas");
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
 app.use(cors());
 CourseRoutes(app);
