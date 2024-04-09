607849858588ac6ef69e927066ccac8181eb3f7f
ami-yokoyama1 <ami.yokoyama1@gmail.com>
2024-04-08 21:17:16 -0700

    first commit on a6

--- App.js
+++ App.js
@@ -2,8 +2,9 @@ import express from 'express';
 import Hello from './Hello.js';
 import Lab5 from './Lab5.js';
 import cors from "cors";
-import CourseRoutes from "./Kanbas/courses/routes.js";
+import CourseRoutes from "./Courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
+import UserRoutes from './Users/routes.js';
 import mongoose from "mongoose";
 
 mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
@@ -14,4 +15,5 @@ app.use(express.json());
 ModuleRoutes(app);
 Lab5(app);
 Hello(app);
+UserRoutes(app);
 app.listen(process.env.PORT ||4000);
