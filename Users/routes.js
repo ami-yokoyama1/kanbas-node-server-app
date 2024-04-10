69e5a2fe278bdcd431656acee293717703d913ad
ami-yokoyama1 <ami.yokoyama1@gmail.com>
2024-04-09 23:38:06 -0700

    more a6

--- Users/routes.js
+++ Users/routes.js
@@ -1,64 +1,56 @@
 import * as dao from "./dao.js";
-
+let currentUser = null;
 export default function UserRoutes(app) {
-  app.get("/api/users", async (req, res) => {
-    // res.send(db.users);
-    const wer = await dao.findAllUsers();
-    res.json(wer);
-  });
-  app.get("/api/users/:userId", async (req, res) => {
-    const userId = req.params.userId;
-    const user = await dao.findUserById(userId);
-    res.send(user);
-  });
-  app.post("/api/users/register", async (req, res) => {
-    console.log("[1] register");
-    const { username, password } = req.body;
-    console.log("[2] username, password", username, password);
+  const createUser = async (req, res) => {
+    const user = await dao.createUser(req.body);
+    res.json(user);
+  };
 
-    const existingUser = await dao.findUserByCredentials(username, password);
-    console.log("[3] existingUser", existingUser);
-    if (existingUser) {
-      res.status(400).send("Username already exists");
-      return;
-    }
-    try {
-      const newUser = await dao.createUser({ username, password }); //{ username, password, _id: Date.now().toString() };
-      console.log("[4] newUser", newUser);
-      // db.users.push(newUser);
-      req.session["currentUser"] = newUser;
-      console.log("[5] req.session", req.session);
-      res.send(newUser);
-    } catch (e) {
-      console.log("Error Creating User");
-    }
-  });
-  app.post("/api/users/profile", async (req, res) => {
-    console.log("[6] profile");
-    console.log("[7] req.session", req.session);
-    if (!req.session.currentUser) {
-      console.log("[8] Not logged in");
-      res.status(401).send("Not logged in");
-      return;
-    }
-    console.log("[9] req.session.currentUser", req.session.currentUser);
-    res.send(req.session.currentUser);
-  });
-  app.post("/api/users/logout", async (req, res) => {
-    req.session.destroy();
-    res.send("Logged out");
-  });
-  app.post("/api/users/login", async (req, res) => {
+  app.post("/api/users", createUser);
+  const deleteUser = async (req, res) => { 
+    const status = await dao.deleteUser(req.params.userId);
+    res.json(status);
+  };
+
+  app.delete("/api/users/:userId", deleteUser);
+  const findAllUsers = async (req, res) => { 
+      const users = await dao.findAllUsers();
+      res.json(users);
+    };
+  app.get("/api/users", findAllUsers);
+  
+  const findUserById = async (req, res) => { 
+    const user = await dao.findUserById(req.params.userId);
+    res.json(user);
+  };
+  app.get("/api/users/:userId", findUserById);
+  
+  const updateUser = async (req, res) => {
+    const { userId } = req.params;
+    const status = await dao.updateUser(userId, req.body);
+    currentUser = await dao.findUserById(userId);
+    res.json(status);
+  };
+
+  const signup = async (req, res) => { };
+
+  const signin = async (req, res) => { 
     const { username, password } = req.body;
-    const ewq = await dao.findUserByCredentials(username, password);
-    // db.users.find(
-    //   (u) => u.username === username && u.password === password
-    // );
-    if (ewq) {
-      req.session.currentUser = ewq;
-      res.send(ewq);
-    } else {
-      res.status(401).send("Invalid credentials");
-    }
-  });
+    currentUser = await dao.findUserByCredentials(username, password);
+    res.json(currentUser);
+  };
+
+  const signout = (req, res) => { };
+
+  const profile = async (req, res) => {
+    res.json(currentUser);
+  };
+  app.post("/api/users/signin", signin);
+  app.post("/api/users/profile", profile);
+  
+  // app.get("/api/users", findAllUsers);
+  app.put("/api/users/:userId", updateUser);
+  app.post("/api/users/signup", signup);
+
+  app.post("/api/users/signout", signout);
 }
